b4e67ce22b882da520d5e3142db9c3015e3ec0f9
Sinan Dogan <sinandogan360@gmail.com>
2025-08-30 14:14:41 +0300

    menu section works perfectly

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -1,104 +1,4 @@
 ﻿
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-
-//public class HomeController : Controller
-//{
-//    private readonly AppDbContext _context;
-
-//    public HomeController(AppDbContext context)
-//    {
-//        _context = context;
-//    }
-
-//    public IActionResult Index
-//    {
-//        get
-//        {
-//            var categories = _context.Category
-//                .Include(c => c.Products)
-//                .Select(c => new MenuViewModel
-//                {
-//                    CategoryId = c.CategoryId,
-//                    CategoryName = c.CategoryName,
-//                    CategoryDescription = c.CategoryDescription,
-//                    Products = c.Products.Select(p => new ProductViewModel
-//                    {
-//                        ProductId = p.ProductId,
-//                        ProductName = p.ProductName,
-//                        Description = p.Description,
-//                        Price = p.Price,
-//                        ImageUrl = p.ImageUrl
-//                    }).ToList()
-//                }).ToList();
-
-//            return View(categories);
-//        }
-
-//    }
-
-
-//}
-
-
-//using System.Linq;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//  // Veritabanı bağlamınızın olduğu alan adını değiştirin
-//// ViewModel'lerinizin olduğu alan adını değiştirin
-
-//public class HomeController : Controller
-//{
-//    private readonly AppDbContext _context; // Veritabanı bağlamınızın adını değiştirin
-
-//    public HomeController(AppDbContext context)
-//    {
-//        _context = context;
-//    }
-
-//    public IActionResult Index()
-//    {
-//        // İşletme bilgilerini al
-//        var business = _context.TblBusiness
-//            .Include(b => b.Address) // Adresleri dahil et
-//            .Include(b => b.Socials) // Sosyal medya bağlantılarını dahil et
-//            .Include(b => b.Menus)
-//            .ThenInclude(m => m.Categories)
-//            .ThenInclude(c => c.Products)
-//            .FirstOrDefault();
-
-//        if (business == null)
-//        {
-//            return NotFound(); // İşletme bilgisi bulunamazsa 404 döndür
-//        }
-
-//        // Adresleri ve sosyal medya bağlantılarını ViewBag'e at
-//        ViewBag.Addresses = business.Address.ToList();
-//        ViewBag.Socials = business.Socials.ToList();
-
-//        // Menü verilerini ViewModel'e dönüştür
-//        var menuViewModels = business.Menus
-//            .SelectMany(m => m.Categories)
-//            .Select(c => new MenuViewModel
-//            {
-//                CategoryId = c.CategoryId,
-//                CategoryName = c.CategoryName,
-//                Products = c.Products.Select(p => new ProductViewModel
-//                {
-//                    ProductId = p.ProductId,
-//                    ProductName = p.ProductName,
-//                    Description = p.Description,
-//                    Price = p.Price,
-//                    ImageUrl = p.ImageUrl
-//                }).ToList()
-//            }).ToList();
-
-//        // Sadece menü verilerini View'a gönder
-//        return View(menuViewModels);
-//    }
-//}
-
-
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -112,21 +12,21 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        //KategoriRepository kategoriRepository = new KategoriRepository();
-        //List<Kategori> kategoriList = new List<Kategori>();
-        //kategoriList=kategoriRepository.Listele().ToList();
-        //ViewBag.KategoriListesi = kategoriList;
-        
-        //UrunRepository urunRepository = new UrunRepository();
-        //List<Urun> urunList = new List<Urun>();
-        //urunList= urunRepository.Listele().ToList();
-        //ViewBag.UrunListesi = urunList;
+        KategoriRepository kategoriRepository = new KategoriRepository();
+        List<Kategori> kategoriList = new List<Kategori>();
+        kategoriList = kategoriRepository.Listele().ToList();
+        ViewBag.KategoriListesi = kategoriList;
 
-        //IletisimBilgileriRepository iletisimBilgileriRepository = new IletisimBilgileriRepository();
-        //List<IletisimBilgileri> iletisimBilgileriList = new List<IletisimBilgileri>();
-        //iletisimBilgileriList = iletisimBilgileriRepository.Listele().ToList();
-        //ViewBag.IletisimBilgileriListesi = iletisimBilgileriList;
-        
+        UrunRepository urunRepository = new UrunRepository();
+        List<Urun> urunList = new List<Urun>();
+        urunList = urunRepository.Listele().ToList();
+        ViewBag.UrunListesi = urunList;
+
+        IletisimBilgileriRepository iletisimBilgileriRepository = new IletisimBilgileriRepository();
+        List<IletisimBilgileri> iletisimBilgileriList = new List<IletisimBilgileri>();
+        iletisimBilgileriList = iletisimBilgileriRepository.Listele().ToList();
+        ViewBag.IletisimBilgileriListesi = iletisimBilgileriList;
+
 
 
         return View();
--- WebApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caed94cca2297bad6ae90d219ba95471df1b6392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbcc6bd256fecb5c87401a48d11b47c07f6f8d51")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
